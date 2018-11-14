1c2a520747a1430953175a5ce58acc211d15885e
Anna Vardanova <annavardanova@Annas-MacBook-Pro.local>
2018-11-14 11:19:19 +0400

    dal starts estrablishhing in core module. Added user entity.

---
+++ note-core/src/main/java/com/note/core/dal/entity/UserEntity.java
@@ -0,0 +1,102 @@
+package com.note.core.dal.entity;
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name="user")
+@Table(name="user")
+public class UserEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="email", unique = true, nullable = false)
+	private String email;
+	
+	@Column(name="password", nullable = false)
+	private String password;
+	
+	@Column(name="created_date",columnDefinition="TIMESTAMP WITH TIME ZONE", nullable = false)
+	private ZonedDateTime createdDate;
+	
+	@Column(name="updated_date",columnDefinition="TIMESTAMP WITH TIME ZONE", nullable = false)
+	private ZonedDateTime updatedDate;
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserEntity other = (UserEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public ZonedDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(ZonedDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public ZonedDateTime getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(ZonedDateTime updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	
+	
+
+}
